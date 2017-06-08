6570088b14e7619fd43050bf031f40dcd01b1a18
Rafal Kaczynsky <rafalkaczynsky@Rafals-MacBook-Pro.local>
2017-06-08 21:18:01 +0100

    Created Basic Wallet screen container

---
+++ src/containers/wallet.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Wallet from '../windows/wallet'
+
+export default class Wallet extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <View>
+                <Wallet />
+            </View>
+        )
+    }
+}  
