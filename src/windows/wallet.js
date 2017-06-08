e53542e730917b47c10208ed91860e7a583132c5
Rafal Kaczynsky <rafalkaczynsky@Rafals-MacBook-Pro.local>
2017-06-08 21:15:43 +0100

    Created basic Wallet screen component

---
+++ src/windows/wallet.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Wallet extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <View>
+                Hello! I'm Wallet Window
+            </View>
+        )
+    }
+}  
