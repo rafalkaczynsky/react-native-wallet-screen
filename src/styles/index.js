555365b8f1e38b3a972cda53b0945b3566fc7b0d
Rafal Kaczynsky <rafalkaczynsky@Rafals-MacBook-Pro.local>
2017-06-10 09:09:42 +0100

    Created basics in Styles

---
+++ src/styles/index.js
@@ -0,0 +1,10 @@
+import _base from './base'
+export const base = _base
+
+import _wallet from './wallet-screen'
+
+
+export default {
+  walletScreen: _wallet,
+  //...//
+}
---
+++ src/styles/wallet-screen.js
@@ -0,0 +1,4 @@
+export default StyleSheet => StyleSheet.extend({
+
+
+})
